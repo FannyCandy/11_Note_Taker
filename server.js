e21489d38b8410824e340cf417543d47a96cc63f
Fanny Tan <fannyc310@gmail.com>
2024-01-07 02:15:20 -0600

    final clean up

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/api/notes/:id', (req, res) => {
       });
   });
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received`);
 
@@ -58,7 +58,8 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
-// DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. 
+// To read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id;
     readFromFile('./db/db.json')
